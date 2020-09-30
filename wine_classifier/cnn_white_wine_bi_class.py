af319c2d6a67f551095eacd2d52a7b17c139a041
zfj <893298592@qq.com>
2020-09-30 18:03:24 +0800

    add cnn and linear model

--- wine_classifier/cnn_white_wine_bi_class.py
+++ wine_classifier/cnn_white_wine_bi_class.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import ipdb
+from collections import Counter
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 from sklearn.preprocessing import StandardScaler    
@@ -13,7 +15,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
 
-df = pd.read_csv("./data/winequality-white.csv",sep=';')
+df = pd.read_csv("../data/winequality-white.csv",sep=';')
 
 df['quality'] = df['quality'].astype('category')
 encode_map = {
@@ -40,8 +42,17 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.fit_transform(X_test)
 
+def shaping_input(*datas):
+    result = []
+    for data in datas:
+        row = data.shape[0]
+        col = data.shape[1]
+        result.append(data.reshape(row, 1, col))
+    return result
 
-EPOCHS = 5000
+X_train, X_test = shaping_input(X_train, X_test)
+
+EPOCHS = 1000
 BATCH_SIZE = 256
 LEARNING_RATE = 1e-4
 
@@ -59,82 +70,42 @@ class trainData(Dataset):
     def __len__ (self):
         return len(self.X_data)
 
-train_data = trainData(torch.FloatTensor(X_train),torch.FloatTensor(y_train))
-## test data    
-class testData(Dataset):
-    
-    def __init__(self, X_data):
-        self.X_data = X_data
-        
-    def __getitem__(self, index):
-        return self.X_data[index]
-        
-    def __len__ (self):
-        return len(self.X_data)
-    
-
-test_data = testData(torch.FloatTensor(X_test))
-
-
+train_data = trainData(torch.FloatTensor(X_train), torch.LongTensor(y_train))
+test_data = trainData(torch.FloatTensor(X_test), torch.LongTensor(y_test))
 train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(dataset=test_data, batch_size=1)
+test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE)
 
-class binaryClassification(nn.Module):
+
+class CNN(nn.Module):
     def __init__(self):
-        super(binaryClassification, self).__init__()
-        self.layer_1 = nn.Linear(11, 64) 
-        self.layer_2 = nn.Linear(64, 64)
-        self.layer_out = nn.Linear(64, 1) 
-        
-        self.relu = nn.ReLU()
-        
-    def forward(self, inputs):
-        x = self.relu(self.layer_1(inputs))
-        x = self.relu(self.layer_2(x))
-        x = self.layer_out(x)
-        
-        return x
-# class binaryClassification(nn.Module):
-#     def __init__(self):
-#         super(binaryClassification, self).__init__()
-#         self.layer_1 = nn.Linear(11, 64) 
-#         self.layer_2 = nn.Linear(64, 64)
-#         self.layer_out = nn.Linear(64, 1) 
-        
-#         self.relu = nn.ReLU()
-#         self.dropout = nn.Dropout(p=0.1)
-#         self.batchnorm1 = nn.BatchNorm1d(64)
-#         self.batchnorm2 = nn.BatchNorm1d(64)
-        
-#     def forward(self, inputs):
-#         x = self.relu(self.layer_1(inputs))
-#         x = self.batchnorm1(x)
-#         x = self.relu(self.layer_2(x))
-#         x = self.batchnorm2(x)
-#         x = self.dropout(x)
-#         x = self.layer_out(x)
-        
-#         return x
+        super(CNN, self).__init__()
 
+        self.conv1 = nn.Conv1d(in_channels = 1,out_channels = 8,kernel_size = 2)
+        self.test_conv2 = nn.Conv1d(8, 16, 2)
+        self.test_conv3 = nn.Conv1d(16, 16, 1)
+        
+        self.fc1 = nn.Linear(16, 2)
+
+    def forward(self, encoder_outputs):
+        cnn_out = F.max_pool1d(F.relu(self.conv1(encoder_outputs)), 2) 
+        cnn_out = F.max_pool1d(F.relu(self.test_conv2(cnn_out)), 2)    
+        cnn_out = F.max_pool1d(F.relu(self.test_conv3(cnn_out)), 2)
+        cnn_out = cnn_out.squeeze()
+        output = self.fc1(cnn_out)
+        return output
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-model = binaryClassification()
+model = CNN()
 model.to(device)
-criterion = nn.BCEWithLogitsLoss()
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-
 def binary_acc(y_pred, y_test):
-    y_pred_tag = torch.round(torch.sigmoid(y_pred))
-
+    y_pred_tag = torch.argmax(y_pred, dim=1)
     correct_results_sum = (y_pred_tag == y_test).sum().float()
     acc = correct_results_sum/y_test.shape[0]
     acc = torch.round(acc * 100)
-    
     return acc
-
 
 for e in range(1, EPOCHS+1):
     model.train()
@@ -145,9 +116,8 @@ for e in range(1, EPOCHS+1):
         optimizer.zero_grad()
         
         y_pred = model(X_batch)
-        
-        loss = criterion(y_pred, y_batch.unsqueeze(1))
-        acc = binary_acc(y_pred, y_batch.unsqueeze(1))
+        loss = criterion(y_pred, y_batch)
+        acc = binary_acc(y_pred, y_batch)
         
         loss.backward()
         optimizer.step()
@@ -159,15 +129,15 @@ for e in range(1, EPOCHS+1):
         print(f'Epoch {e+0:04}: | Loss: {epoch_loss/len(train_loader):.5f} | Acc: {epoch_acc/len(train_loader):.3f}')
 
     if e % 100 == 0:
-        y_pred_list = []
         model.eval()
+        epoch_acc = 0
         with torch.no_grad():
-            for X_batch in test_loader:
+            for X_batch, y_batch in test_loader:
                 X_batch = X_batch.to(device)
                 y_test_pred = model(X_batch)
-                y_test_pred = torch.sigmoid(y_test_pred)
-                y_pred_tag = torch.round(y_test_pred)
-                y_pred_list.append(y_pred_tag.cpu().numpy())
+                acc = binary_acc(y_test_pred, y_batch)
+                epoch_acc += acc.item()
+        print(f'Test Acc: {epoch_acc/len(test_loader):.3f}')
 
-        y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
-        print(classification_report(y_test, y_pred_list))
+# Epoch 1000: | Loss: 0.48713 | Acc: 76.923
+# Test Acc: 74.143
