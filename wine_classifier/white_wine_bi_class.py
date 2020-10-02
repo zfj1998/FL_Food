e9113008c4acb9fd6581c17924e8f7383687813c
zfj <893298592@qq.com>
2020-10-02 10:06:49 +0800

    add batchnorm

--- wine_classifier/white_wine_bi_class.py
+++ wine_classifier/white_wine_bi_class.py
@@ -79,21 +79,6 @@ test_data = testData(torch.FloatTensor(X_test))
 train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(dataset=test_data, batch_size=1)
 
-class binaryClassification(nn.Module):
-    def __init__(self):
-        super(binaryClassification, self).__init__()
-        self.layer_1 = nn.Linear(11, 64) 
-        self.layer_2 = nn.Linear(64, 64)
-        self.layer_out = nn.Linear(64, 1) 
-        
-        self.relu = nn.ReLU()
-        
-    def forward(self, inputs):
-        x = self.relu(self.layer_1(inputs))
-        x = self.relu(self.layer_2(x))
-        x = self.layer_out(x)
-        
-        return x
 # class binaryClassification(nn.Module):
 #     def __init__(self):
 #         super(binaryClassification, self).__init__()
@@ -102,19 +87,34 @@ class binaryClassification(nn.Module):
 #         self.layer_out = nn.Linear(64, 1) 
         
 #         self.relu = nn.ReLU()
-#         self.dropout = nn.Dropout(p=0.1)
-#         self.batchnorm1 = nn.BatchNorm1d(64)
-#         self.batchnorm2 = nn.BatchNorm1d(64)
         
 #     def forward(self, inputs):
 #         x = self.relu(self.layer_1(inputs))
-#         x = self.batchnorm1(x)
 #         x = self.relu(self.layer_2(x))
-#         x = self.batchnorm2(x)
-#         x = self.dropout(x)
 #         x = self.layer_out(x)
         
 #         return x
+class binaryClassification(nn.Module):
+    def __init__(self):
+        super(binaryClassification, self).__init__()
+        self.layer_1 = nn.Linear(11, 64) 
+        self.layer_2 = nn.Linear(64, 64)
+        self.layer_out = nn.Linear(64, 1) 
+        
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(64)
+        self.batchnorm2 = nn.BatchNorm1d(64)
+        
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+        
+        return x
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
